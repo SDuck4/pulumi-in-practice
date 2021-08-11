3c0da3da48aa80efa8f798610fdeda3e6a6655d1
SDuck4 <iam@sduck.is>
2021-08-11 11:36:15 +0900

    Parse `openapi.json`

--- go-openapi-spec-to-ingress/main.go
+++ go-openapi-spec-to-ingress/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+
+	"github.com/buger/jsonparser"
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	networkingv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/networking/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -49,7 +55,7 @@ func main() {
 		}
 
 		// Create `petstore-svc`
-		_, err = corev1.NewService(ctx, "petstore-svc", &corev1.ServiceArgs{
+		petSvc, err := corev1.NewService(ctx, "petstore-svc", &corev1.ServiceArgs{
 			Metadata: metav1.ObjectMetaArgs{
 				Name: pulumi.String("petstore-svc"),
 			},
@@ -66,6 +72,42 @@ func main() {
 		if err != nil {
 			return err
 		}
+
+		// Parse `openapi.json`
+		oas, err := ioutil.ReadFile("openapi.json")
+		if err != nil {
+			return err
+		}
+		prefix, err := jsonparser.GetString(oas, "servers", "[0]", "url")
+		if err != nil {
+			return err
+		}
+		pathParamRe := regexp.MustCompile(`\{.+\}`)
+		pathParamMap := map[string]string{
+			"{petId}":    `(\d+)`,
+			"{orderId}":  `(\d+)`,
+			"{username}": `([A-Za-z0-9]+)`,
+		}
+		paths := networkingv1.HTTPIngressPathArray{}
+		jsonparser.ObjectEach(oas, func(key, value []byte, dataType jsonparser.ValueType, offset int) error {
+			path := fmt.Sprintf("%s%s", prefix, key)
+			path = pathParamRe.ReplaceAllStringFunc(path, func(s string) string {
+				return pathParamMap[s]
+			})
+			paths = append(paths, networkingv1.HTTPIngressPathArgs{
+				Path:     pulumi.String(path),
+				PathType: pulumi.String("Exact"),
+				Backend: networkingv1.IngressBackendArgs{
+					Service: networkingv1.IngressServiceBackendArgs{
+						Name: petSvc.Metadata.Elem().Name().Elem(),
+						Port: networkingv1.ServiceBackendPortArgs{
+							Number: petSvc.Spec.Elem().Ports().Index(pulumi.Int(0)).Port(),
+						},
+					},
+				},
+			})
+			return nil
+		}, "paths")
 
 		return nil
 	})
